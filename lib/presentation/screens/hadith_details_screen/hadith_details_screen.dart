e5caffd2e34fe85628f8a9ce3e4aca0113f2cd74
7oda <7oda@gmail.com>
2024-10-10 00:20:50 +0300

    + MakeHadithdetailsScreenCenter

--- lib/presentation/screens/hadith_details_screen/hadith_details_screen.dart
+++ lib/presentation/screens/hadith_details_screen/hadith_details_screen.dart
@@ -32,79 +32,81 @@ class HadithDetailsScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(
             horizontal: 8,
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Card(
-                  shadowColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  color: Theme.of(context).primaryColor.withOpacity(0.7),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12,
-                      horizontal: 8,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    shadowColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
                     ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
+                    color: Theme.of(context).primaryColor.withOpacity(0.7),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 8,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            model.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          const Divider(
+                            color: Colors.white,
+                            thickness: 2,
+                            indent: 28,
+                            endIndent: 28,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 12,
+                              horizontal: 28,
+                            ),
+                            child: Text(
+                              model.content,
+                              textAlign: TextAlign.center,
+                              textDirection: TextDirection.rtl,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20, top: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          model.name,
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelSmall
-                              ?.copyWith(color: Colors.white),
-                        ),
-                        const Divider(
-                          color: Colors.white,
-                          thickness: 2,
-                          indent: 28,
-                          endIndent: 28,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 12,
-                            horizontal: 28,
+                        if (model.index != 0)
+                          ArrowWidget(
+                            icon: Icons.arrow_back_ios_outlined,
+                            index: model.index,
+                            ahaditList: model.ahadithList,
+                            next: false,
                           ),
-                          child: Text(
-                            model.content,
-                            textAlign: TextAlign.center,
-                            textDirection: TextDirection.rtl,
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
+                        const SizedBox(
+                          width: 40,
                         ),
+                        if (model.index != 49)
+                          ArrowWidget(
+                            icon: Icons.arrow_forward_ios_outlined,
+                            index: model.index,
+                            ahaditList: model.ahadithList,
+                            next: true,
+                          ),
                       ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20, top: 12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (model.index != 0)
-                        ArrowWidget(
-                          icon: Icons.arrow_back_ios_outlined,
-                          index: model.index,
-                          ahaditList: model.ahadithList,
-                          next: false,
-                        ),
-                      const SizedBox(
-                        width: 40,
-                      ),
-                      if (model.index != 49)
-                        ArrowWidget(
-                          icon: Icons.arrow_forward_ios_outlined,
-                          index: model.index,
-                          ahaditList: model.ahadithList,
-                          next: true,
-                        ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
